8553e914e2973218373086dba1c4c63e74827e52
JM <dwm0816@outlook.com>
2021-02-20 14:57:05 -0800

    button and counter functionality

--- console.js
+++ console.js
@@ -11,4 +11,51 @@ let findYear=(d)=>{
     };
 };
 findYear(date);
+////////////////////////////////////////////////////
+let m1 = document.getElementById("m1");
+let m5 = document.getElementById("m5");
+let m10 = document.getElementById("m10");
+let p1 = document.getElementById("p1");
+let p5 = document.getElementById("p5");
+let p10 = document.getElementById("p10");
+let counter = document.getElementById("countNum");
+let count = 0;
 
+let increaseNum = (num) =>{
+    count += num;
+    updateCounter(count);
+
+}
+
+let decreaseNum = (num) => {
+    count -= num;
+    updateCounter(count);
+}
+////////////////////////////////////////////
+m1.onclick = function () {
+    decreaseNum(1);
+}
+
+m5.onclick = function () {
+    decreaseNum(5);
+}
+
+m10.onclick = function () {
+    decreaseNum(10);
+}
+
+p1.onclick = function () {
+    increaseNum(1);
+}
+
+p5.onclick = function () {
+    increaseNum(5);
+}
+
+p10.onclick = function () {
+    increaseNum(10);
+}
+
+let updateCounter = (num) => {
+    counter.innerText = num;
+}
